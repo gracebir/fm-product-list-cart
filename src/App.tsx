48aabe043cbafbda6e42b00959f65260c49e876e
gracebir <grace.devolop@gmail.com>
2024-12-03 14:07:03 +0200

    adding some assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 /** @format */
 
 function App() {
-    return <></>;
+    return (
+        <div className='w-full min-h-screen bg-color-rose-100 font-red-hat'>
+            <div className='max-w-7xl mx-auto px-4 md:px-6'></div>
+        </div>
+    );
 }
 
 export default App;
